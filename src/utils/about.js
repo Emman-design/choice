b57734c010065316b82a5ce0a71096673c1adc27
Emmanuel Akoja <57469492+Emman-design@users.noreply.github.com>
2022-09-29 14:44:45 -0500

    Update about.js

--- src/utils/about.js
+++ src/utils/about.js
@@ -2,4 +2,4 @@ export const bio = `I am a Ph.D student of Computer Science at Louisiana State
               University. I work with Dr. Andrew Webb on mobile augmented reality
               to that improve interaction and well-being. My research interests include virtual reality, augmented reality, mixed reality.
               Currently, I am expanding my knowledge in the above listed areas. 
-              Meaniwhile, I have a vast knowledge and experience in the area of data analysis and visualization;
+              Meaniwhile, I have a vast knowledge and experience in the area of data analysis and visualization';
