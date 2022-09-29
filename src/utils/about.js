f90cb6b2784e239c55364f37ae7c1ab24947d840
Emmanuel Akoja <57469492+Emman-design@users.noreply.github.com>
2022-09-29 14:38:23 -0500

    Update about.js

--- src/utils/about.js
+++ src/utils/about.js
@@ -1,7 +1,5 @@
 export const bio = `I am a Ph.D student of Computer Science at Louisiana State
-              University. I work with Dr. Nash Mahmoud on software privacy and
-              app store analysis at SEEL. My research interests include mobile
-              app privacy, app store analysis, natural language processing, and
-              requirement engineering. My research is supported by the U.S.
-              National Science Foundation (Award CNS 1951411) and LSU Economic
-              Development Assistantships awards.`;
+              University. I work with Dr. Andrew Webb on mobile augmented reality
+              to that improve interaction and well-being. My research interests include virtual reality, augmented reality, mixed reality.
+              Currently, I am expanding my knowledge in the above listed areas. 
+              Meaniwhile, I have a vast knowledge and experience in the area of data analysis and visualization;
