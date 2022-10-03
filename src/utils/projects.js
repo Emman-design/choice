b37b77b8bc2f56ae0fc5ffe4043d5f2bd0c1eee6
Emmanuel Akoja <57469492+Emman-design@users.noreply.github.com>
2022-10-03 13:43:49 -0500

    Update projects.js

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -1,25 +1,14 @@
 export const projects = [
   {
     title:
-      'Unsupervised Summarization of Privacy Concerns in Mobile Application Reviews',
-    base: "F. Ebrahimi, and A. Mahmoud, International Conference on Automated Software Engineering (ASE'22)",
+      'A Review: Machine Learning and Artificial Intelligence for Organ Transplant Survival Prediction',
+    base: "Emmanuel Akoja, Computer Research Class (Spring'22)",
     link: '#',
   },
   {
     title:
-      'A Systematic Literature Review of Anti-Discrimination Design Strategies in the Digital Sharing Economy',
-    base: "M. Tushev, F. Ebrahimi, and A. Mahmoud, IEEE Transactions on Software Engineering (TSE'22)",
+      'Cloud Computing Security and Solutions: Survey',
+    base: "Emmanuel Akoja, Advance Storage System (Fall'20)",
     link: '#',
-  },
-  {
-    title:
-      'Domain-Specific Analysis of Mobile App Reviews Using Keyword-Assisted Topic Models',
-    base: "M. Tushev, F. Ebrahimi, and A. Mahmoud, International Conference on Software Engineering (ICSE'22)",
-    link: '#',
-  },
-  {
-    title: 'Classifying Mobile Applications using Word Embedding ',
-    base: "F. Ebrahimi, M. Tushev, and A. Mahmoud, ACM Transactions on Software Engineering and Methodology (TOSEM'21)",
-    link: '#',
-  },
+  }
 ];
