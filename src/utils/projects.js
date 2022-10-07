4c23c6e3f5c96f009882609e3a7f9ad5436ad684
Emmanuel Akoja <57469492+Emman-design@users.noreply.github.com>
2022-10-07 04:55:37 -0500

    Update projects.js

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -3,7 +3,15 @@ export const projects = [
     title:
       'A Review: Machine Learning and Artificial Intelligence for Organ Transplant Survival Prediction',
     base: "Emmanuel Akoja, Computer Research Class (Spring'22)",
-    link: '/pdf/CSC7700_Project_1_Emmanuel_Akoja_Wrtiting_Project%20(1).pdf',
+    <Link
+              href="/pdf/CSC7700_Project_1_Emmanuel_Akoja_Wrtiting_Project%20(1).pdf"
+              isExternal
+              color="blue.400"
+              download="Project"
+              fontWeight="bold"
+            >
+              Link
+            </Link>,
   },
   {
     title:
