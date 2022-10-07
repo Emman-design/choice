e436e5ee8119deee4fcb4c26ea86c1ecbeae1e04
Emmanuel Akoja <57469492+Emman-design@users.noreply.github.com>
2022-10-07 04:37:23 -0500

    Update news.js

--- src/utils/news.js
+++ src/utils/news.js
@@ -1,13 +1,25 @@
 export const newsData = [
   {
-    title: 'Big Data and Machine Learning in Cognitive Surgical Robotics',
-    desc: 'Emmanuel Akoja, International Conference on Big Data and Smart Computing',
-    link: '#',
+    title: '09/27/22 Paper Submitted',
+    desc: 'IEEE BigComp2023',
+    link: 'http://www.bigcomputing.org/',
   },
 
   {
-    title: 'Big Data and Machine Learning in Cognitive Surgical Robotics',
-    desc: 'Emmanuel Akoja, International Conference on Big Data and Smart Computing',
-    link: '#',
+    title: '10/10-13/22 Global Gaming Expo',
+    desc: 'G2E 2022 in Las Vegas',
+    link: 'https://unity.com/events-hub',
+  },
+  
+  {
+    title: '11/01/22 Unite 2022',
+    desc: 'inspiration, learning, and connecting',
+    link: 'https://unity.com/events-hub',
+  },
+  
+  {
+    title: '11/02/22 ',
+    desc: 'Unity for Humanity Summit 2022',
+    link: 'https://unity.com/events-hub',
   },
 ];
